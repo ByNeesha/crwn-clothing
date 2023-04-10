5d96804692c6f3e38158c1d218f10a8beb095b97
ByNeesha <neesha@live.nl>
2023-04-10 16:08:56 +0200

    redux-persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import { compose, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { legacy_createStore as createStore } from "redux";
-// import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
 
 const loggerMiddleware = (store) => (next) => (action) => {
@@ -14,7 +15,21 @@ const loggerMiddleware = (store) => (next) => (action) => {
   next(action);
   console.log("next state: ", store.getState());
 };
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const middleWares = [loggerMiddleware];
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
