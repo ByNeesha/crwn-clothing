c8a4b92985920cbe502ce052c3aec42f8aebc3ae
ByNeesha <neesha@live.nl>
2023-04-11 17:57:42 +0200

    Revert "Revert "sagas""
    This reverts commit 649de01a8ccc2d577f6260e9e917ff0439cb13f9.

--- src/store/store.js
+++ src/store/store.js
@@ -4,18 +4,23 @@ import storage from "redux-persist/lib/storage";
 import { legacy_createStore as createStore } from "redux";
 import { rootReducer } from "./root-reducer";
 import logger from "redux-logger";
+import createSagaMiddleware from "redux-saga";
+import { rootSaga } from "./root-saga";
 
 const persistConfig = {
   key: "root",
   storage,
-  blacklist: ["user"],
+  whitelist: ["cart"],
 };
+
+const sagaMiddleware = createSagaMiddleware();
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
-  Boolean
-);
+const middleWares = [
+  process.env.NODE_ENV !== "production" && logger,
+  sagaMiddleware,
+].filter(Boolean);
 
 const composeEnhancer =
   (process.env.NODE_ENV !== "production" &&
@@ -29,5 +34,7 @@ export const store = createStore(
   undefined,
   composedEnhancers
 );
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
