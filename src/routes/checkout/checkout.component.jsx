e7a0d2cb27c54775ae833d2a78a7c19143a314e6
ByNeesha <neesha@live.nl>
2023-04-11 22:37:58 +0200

    Revert "stripe"
    This reverts commit 9955ff6b557f9b33d2f293612e3973baac676ea6.

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -4,7 +4,6 @@ import {
   selectCartTotal,
 } from "../../store/cart/cart.selector";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
-import PaymentForm from "../../components/payment-form/payment-form-component";
 import {
   CheckoutContainer,
   CheckoutHeader,
@@ -38,7 +37,6 @@ const Checkout = () => {
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
       <Total>Total: €{cartTotal}</Total>
-      <PaymentForm />
     </CheckoutContainer>
   );
 };
